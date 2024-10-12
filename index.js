2369d79c97cc604dbdcd0bfb1819da1c9e43d79c
MauricioTdM <mauricio1tmelo@yahoo.com.br>
2024-10-12 19:33:51 -0300

    Entrega do desafio DIO

---
+++ index.js
@@ -0,0 +1,32 @@
+let nome = "Hércules"
+let qtdXp = 10001
+let nivel = ""
+
+switch (true) {
+    case qtdXp <= 1000:
+        nivel = "Ferro"
+        break;
+    case qtdXp >= 1001 && qtdXp <= 2000:
+        nivel = "Bronze"
+        break;
+    case qtdXp >= 2001 && qtdXp <= 5000:
+        nivel = "Prata"
+        break;
+    case qtdXp >= 5001 && qtdXp <= 7000:
+        nivel = "Ouro"
+        break;
+    case qtdXp >= 7001 && qtdXp <= 8000:
+        nivel = "Platina"
+        break;
+    case qtdXp >= 8001 && qtdXp <= 9000:
+        nivel = "Ascendente"
+        break;
+    case qtdXp >= 9001 && qtdXp <= 10000:
+        nivel = "Imortal"
+        break;
+    case qtdXp >= 10001:
+        nivel = "Radiante"
+        break;
+}
+
+console.log(`O herói de nome ${nome} está no nível de ${nivel}.`)
